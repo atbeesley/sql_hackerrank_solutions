c55b1a975c3ebf7788ff531e1ff1c2460a1933cf
atbeesley <alexander.beesley@gmail.com>
2020-06-30 10:07:54 +0100

    adds employee_earnings

---
+++ earnings_of_employees.sql
@@ -0,0 +1,14 @@
+-- Write a query to find the maximum total earnings for all employees, 
+
+-- as well as the total number of employees who have maximum total earnings. 
+
+
+-- Then print these values as 2 space-separated integers.
+
+
+
+SELECT MAX(SALARY*MONTHS),
+COUNT(*) FROM EMPLOYEE
+WHERE SALARY*MONTHS=
+(SELECT MAX(SALARY*MONTHS)
+FROM EMPLOYEE);
